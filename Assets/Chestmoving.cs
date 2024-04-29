bf0c8c3266a34e7a5edd930ddece5356de7f6dd9
Luke Whitehead <Luke2.whitehead@live.uwe.ac.uk>
2024-04-29 23:38:33 +0100

    Added chest into game

---
+++ Assets/Chestmoving.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chestmoving : MonoBehaviour
+{
+
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "player")
+        {
+            animator.SetTrigger("Open");
+        }
+
+        
+    }
+
+
+}
